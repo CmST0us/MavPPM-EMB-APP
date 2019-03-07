0b946d6180f1bf169d7c14192bbcc62c9bdd9a21
eric3u <eric3u@outlook.com>
2019-03-07 14:34:41 +0800

    [Refactor] 使用stringstream替代strstream

--- src/app/mavlink/mavlink_protocol.cpp
+++ src/app/mavlink/mavlink_protocol.cpp
@@ -2,7 +2,7 @@
 // Created by eric3u on 19-3-7.
 //
 
-#include <strstream>
+#include <sstream>
 #include "mavlink_protocol.hpp"
 
 int mavppm::MavlinkProtocol::SYS_ID_IOS = MAVPPM_SYSTEM_ID_IOS;
@@ -64,7 +64,7 @@ std::shared_ptr<mavppm::utils::Data> mavppm::MavlinkProtocol::serialize(mavlink_
 
 std::string mavppm::MavlinkProtocol::messageDescription(mavlink_message_t &message) {
     mavlink_message_info_t info = mavppm::MavlinkProtocol::mavlinkMessageInfo(message.msgid);
-    std::ostrstream sstr;
+    std::stringstream sstr;
     sstr<<info.name<<":"<<std::endl;
     for (int i = 0; i < info.num_fields; ++i) {
         mavlink_field_info_t field = info.fields[i];
@@ -74,57 +74,57 @@ std::string mavppm::MavlinkProtocol::messageDescription(mavlink_message_t &messa
         switch (field.type) {
             case MAVLINK_TYPE_CHAR: {
                 char *v = (char *) valueAddr;
-                sstr<<std::to_string(*v);
+                sstr << std::to_string(*v);
             }
                 break;
             case MAVLINK_TYPE_UINT8_T: {
                 uint8_t *v = (uint8_t *) valueAddr;
-                sstr<<std::to_string(*v);
+                sstr << std::to_string(*v);
             }
                 break;
             case MAVLINK_TYPE_INT8_T: {
                 int8_t *v = (int8_t *) valueAddr;
-                sstr<<std::to_string(*v);
+                sstr << std::to_string(*v);
             }
                 break;
             case MAVLINK_TYPE_UINT16_T: {
                 uint16_t *v = (uint16_t *) valueAddr;
-                sstr<<std::to_string(*v);
+                sstr << std::to_string(*v);
             }
                 break;
             case MAVLINK_TYPE_INT16_T: {
                 int16_t *v = (int16_t *) valueAddr;
-                sstr<<std::to_string(*v);
+                sstr << std::to_string(*v);
             }
                 break;
             case MAVLINK_TYPE_UINT32_T: {
                 uint32_t *v = (uint32_t *) valueAddr;
-                sstr<<std::to_string(*v);
+                sstr << std::to_string(*v);
             }
                 break;
             case MAVLINK_TYPE_INT32_T: {
                 int32_t *v = (int32_t *) valueAddr;
-                sstr<<std::to_string(*v);
+                sstr << std::to_string(*v);
             }
                 break;
             case MAVLINK_TYPE_UINT64_T: {
                 uint64_t *v = (uint64_t *) valueAddr;
-                sstr<<std::to_string(*v);
+                sstr << std::to_string(*v);
             }
                 break;
             case MAVLINK_TYPE_INT64_T: {
                 int64_t *v = (int64_t *) valueAddr;
-                sstr<<std::to_string(*v);
+                sstr << std::to_string(*v);
             }
                 break;
             case MAVLINK_TYPE_FLOAT: {
                 float_t *v = (float_t *) valueAddr;
-                sstr<<std::to_string(*v);
+                sstr << std::to_string(*v);
             }
                 break;
             case MAVLINK_TYPE_DOUBLE: {
                 double_t *v = (double_t *) valueAddr;
-                sstr<<std::to_string(*v);
+                sstr << std::to_string(*v);
             }
                 break;
         }
